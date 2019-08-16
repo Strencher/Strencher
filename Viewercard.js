19421bd7d3409d8b601b525b506e0e5ab928bcbe
Strencher <46447572+Strencher@users.noreply.github.com>
2019-08-16 19:20:42 +0200

    Add files via upload

---
+++ Viewercard.js
@@ -0,0 +1,6 @@
+document.getElementById('openuser').addEventListener('click',function(){
+usernamen=document.getElementById('namen').value;
+window.open('https://www.twitch.tv/popout/shoxx__/viewercard/'+usernamen+'');
+/*oder*/
+// location.href='https://www.twitch.tv/popout/shoxx__/viewercard/'+usernamen+'';
+});
