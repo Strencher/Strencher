271de805ea5de34ad7e6194a7256d6269dea1cc8
Strencher <46447572+Strencher@users.noreply.github.com>
2021-02-15 18:58:02 +0100

    clean

--- Viewercard.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('openuser').addEventListener('click',function(){
-usernamen=document.getElementById('namen').value;
-window.open('https://www.twitch.tv/popout/shoxx__/viewercard/'+usernamen+'');
-/*oder*/
-// location.href='https://www.twitch.tv/popout/shoxx__/viewercard/'+usernamen+'';
-});
